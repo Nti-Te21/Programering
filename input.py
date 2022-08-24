91a8d46d34d2d3ecd8bb608c02639315323a4599
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2022-08-24 10:40:40 +0200

    30 :(

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("Vad heter du? ")
+print("Välkommen tilbaks", name)
+age = int(input ("Hur gammal är du? "))
+print(name,age)
+age30 = 30 - age
+if age30 >= 0:
+    print(" Visste du att om",age30, "år är du 30 år gammal?" )
+else:
+    print("Du är redan över 30 år gammal :(")
