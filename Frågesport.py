af207e1c673328002aebba6bbeb84ede74d4ae8d
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2022-08-29 13:28:46 +0200

    Frågesport klar

--- Frågesport.py
+++ Frågesport.py
@@ -1,31 +1,31 @@
 Antalrätt=0
-svar1 = input("Vad är Sveriges huvudstad? ")
-if svar1 := "Stockholm":
+svar1 = input("Vad är Sveriges huvudstad? ").capitalize()
+if svar1 == "Stockholm":
     print("Rätt!")
     Antalrätt=Antalrätt+1
 else:
     print("Fel!")
-svar2 = input("Vad är Norges huvudstad? ")
-if svar2 := "Oslo":
+svar2 = input("Vad är Norges huvudstad? ").capitalize()
+if svar2 == "Oslo":
     print("Rätt!")
     Antalrätt=Antalrätt+1
 else:
     print("Fel!")
-svar3 = input("Vad är Bulgariens huvudstad? ")
-if svar3 := "Sofia":
+svar3 = input("Vad är Bulgariens huvudstad? ").capitalize()
+if svar3 == "Sofia":
     print("Rätt!")
     Antalrätt=Antalrätt+1
 else:
     print("Fel!")
-svar4 = input("Vad är Turkiets huvudstad? ")
-if svar4 := "Ankara":
+svar4 = input("Vad är Turkiets huvudstad? ").capitalize()
+if svar4 == "Ankara":
     print("Rätt!")
     Antalrätt=Antalrätt+1
 else:
     print("Fel!")
-if Antalrätt := 4:
+if Antalrätt == 4:
     print("Bra jobbat! Alla rätt 4/4.")
-elif Antalrätt == [1,2,3]:
-    print("Några rätt iallafall bättre lycka nästa gång")
+elif Antalrätt == 1 or Antalrätt == 2 or Antalrätt == 3:
+    print(f"Några rätt iallafall bättre lycka nästa gång {Antalrätt}/4")
 else:
     print("Alla fel! Bättre kan du! Försök igen!")
