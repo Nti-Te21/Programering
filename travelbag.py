0cd2f55abdfd45afbbb7664acc03df28f2771277
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2022-09-14 11:08:41 +0200

    travelbag 1.0

--- travelbag.py
+++ travelbag.py
@@ -1,22 +1,26 @@
-# Travelbag
-# Skelettkod till uppgiften
-
-travelbag = []
+travelbag = ["hej","då"]
 
 while True:
-   menyval = input("1. Kolla i resväskan\n"
+
+   menyval = input("\n1. Kolla i resväskan\n"
                    "2. Lägg sak i resväskan\n"
                    "3. Ta bort sak i resväskan\n"
-                   "Avsluta program")
-
+                   "4. Avsluta program ")
    if menyval == "1":
-       print(*travelbag[])
+    travelbag.sort()
+    for i,sak in enumerate(travelbag, start=1):
+        print (i, sak)
 
    elif menyval == "2":
-       pass
+       nya_saker = input("Lägg till en till sak ").split(" ")
+       travelbag+= nya_saker
 
    elif menyval == "3":
-       pass
-
+       ta_bort = input("Vad vill du ta bort? ")
+       if ta_bort in travelbag:
+          travelbag.remove(ta_bort)
+       else:
+        print("inte i resväskan")
+    
    elif menyval == "4":
        break
